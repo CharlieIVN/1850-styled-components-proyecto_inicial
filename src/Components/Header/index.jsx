55381851f15e38fb3e59768f0b87fe1cd1268bd8
Charlie <brinkcore@gmail.com>
2023-05-04 02:02:01 -0600

    first

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../../assets/images/logo.svg";
+import { colorPrimario } from "../UI/variables";
+
+const StyledHeader = styled.nav`
+  background-color: ${colorPrimario};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`
+
+const Logo =styled.img`
+  height: 50px;
+  width: 50px;
+
+
+`
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <Logo src={logo} alt="Logo Smart Bank" />
+      <div>
+        <a className="btn-secundario" href="https://google.com">
+          Ayuda
+        </a>
+        <a className="btn-primario" href="https://google.com">
+          Salir
+        </a>
+      </div>
+    </StyledHeader>
+  );
+};
+
+export default Header;
---
+++ src/Components/UI/variables.js
@@ -0,0 +1,3 @@
+export const colorPrimario = '#41d3be';
+export const fondoClaro = '#f1f1f11';
+export const textoFondoClaro = 'grey';

f351ee2e1c1f1df2f3feebc91e920728b1a39d7a
Charlie <brinkcore@gmail.com>
2023-05-05 01:42:17 -0600

    03-02 refac

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-
+import styled from "styled-components"
 import Title from "../Title";
 import Account from "../Account";
 
+
+const StyledContainer = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+const StylesContent = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
 const Container = () => {
   return (
-    <div className="container">
+    <StyledContainer>
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StylesContent>
         <Account />
-      </section>
-    </div>
+      </StylesContent>
+    </StyledContainer>
   );
 };
 
